20c603e4f9b268b217ffae8213243654c80317f6
Pepetritenisgoosenagasty <adjalokoedward@gmail.com>
2022-02-18 23:54:34 +0000

    Updates

--- index.js
+++ index.js
@@ -3,12 +3,41 @@ const cors = require("cors");
 
 const authRoute = require('./routes/auth.js');
 
+const accountSid = process.env.TWILLIO_ACCOUNT_SID;
+const authToken = process.env.TWILLIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.MESSAGEING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken)
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body;
+
+    if(type === 'message.new') {
+        members
+            .filter((member) => member.user_id !== sender.id)
+            .forEach(({ user }) => {
+                if(!user.online) {
+                    twilioClient.messages.create({
+                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+                        messagingServiceSid: messagingServiceSid,
+                        to: user.phoneNumber
+                    })
+                        .then(() => console.log('Message sent!'))
+                        .catch((err) => console.log(err));
+                }
+            })
+
+            return res.status(200).send('Message sent!');
+    }
+
+    return res.status(200).send('Not a new message request');
+});
 
 
 app.get('/', (req, res) => {
